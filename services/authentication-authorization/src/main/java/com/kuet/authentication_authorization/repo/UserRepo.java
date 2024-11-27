2310522a7dbdbab709082c4b2702ec8559cadd26
sakib <sakiburrahman11.msr@gmail.com>
2024-11-27 18:50:21 +0600

    Authentication start

--- services/authentication-authorization/src/main/java/com/kuet/authentication_authorization/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.kuet.authentication_authorization.repo;
-
-import com.kuet.authentication_authorization.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<Users,Integer>{
-
-    Users findByUsername(String username);
-}
--- services/chat/src/main/java/com/kuet/chat/chat/MessageType.java
+++
@@ -1,8 +0,0 @@
-package com.kuet.chat.chat;
-
-public enum MessageType {
-
-    CHAT,
-    JOIN,
-    LEAVE
-}

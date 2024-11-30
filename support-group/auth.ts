17a33d5cf5ce2f67ab145a0e88959d39f236de56
sakib <sakiburrahman11.msr@gmail.com>
2024-11-30 19:45:20 +0600

    Prisma setup for nextauth

--- support-group/auth.ts
+++ support-group/auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
+import{ PrismaAdapter } from "@auth/prisma-adapter"
+import { db } from "@/lib/db"
+import authConfig from "./auth.config"
  
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub, Google],
+  adapter: PrismaAdapter(db ),
+  session: { strategy: "jwt" },
+  ...authConfig,
 })

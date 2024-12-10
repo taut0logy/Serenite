5f53519405be407f123bf7a3333123a5955a282d
sakib <sakiburrahman11.msr@gmail.com>
2024-12-10 18:26:22 +0600

    Sign out

--- support-group/app/(protected)/settings/page.tsx
+++ support-group/app/(protected)/settings/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 const SettingsPage = async () => {
 
@@ -8,6 +8,15 @@ const SettingsPage = async () => {
   return (
     <div>
       {JSON.stringify(session)}
+      <form action={async () => {
+        "use server";
+
+        await signOut();
+      }}>
+        <button type="submit" >
+          Sign out
+        </button>
+      </form>
     </div>
   );
 }

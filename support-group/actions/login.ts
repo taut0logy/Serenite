776f0b222fb2f97d659ef11ca98056ed5b95a140
sakib <sakiburrahman11.msr@gmail.com>
2024-12-10 17:27:45 +0600

    Login session created

--- support-group/actions/login.ts
+++ support-group/actions/login.ts
@@ -1,13 +1,41 @@
 "use server";
-import * as z from "zod";	
+import * as z from "zod";
 import { LoginSchema } from "@/schemas";
+import { signIn } from "@/auth";
+import { signOut } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.parse(values);
 
-  if(!validatedFields) {
-    return {error: "Invalid fields"};
+  if (!validatedFields) {
+    return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password } = validatedFields;
+
+  try {
+    await signIn("credentials", {
+      email, password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials" };
+
+        default:
+          return { error: "Something went wrong !" };
+
+      }
+    }
+
+    throw error;
+
+
+
+  }
+
 };
--- support-group/auth.ts
+++ support-group/auth.ts
@@ -4,8 +4,10 @@ import { db } from "@/lib/db"
 import authConfig from "./auth.config"
  
 export const {
-  handlers : {GET, POST},
+  handlers : { GET, POST },
    auth,
+   signIn,
+   signOut,
   } = NextAuth({
   adapter: PrismaAdapter(db ),
   session: { strategy: "jwt" },

894f027dd5d5dc55c1cf4f3cce9058dcd5b287b4
sakib <sakiburrahman11.msr@gmail.com>
2024-12-11 14:48:07 +0600

    Add new password functionality with validation and form handling

--- support-group/schemas/index.ts
+++ support-group/schemas/index.ts
@@ -1,5 +1,14 @@
 import * as z from "zod";
 
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6,
+    {
+      message: "Minimum of 6 characters required",
+    }
+  ),
+
+});
+
 export const ResetSchema = z.object({
   email: z.string().email(
     {
@@ -15,7 +24,7 @@ export const LoginSchema = z.object({
       message: "Email is required",
     }
   ),
-  password: z.string().min(1,{
+  password: z.string().min(1, {
     message: "Password is required",
   }),
 });
@@ -26,7 +35,7 @@ export const RegisterSchema = z.object({
       message: "Email is required",
     }
   ),
-  password: z.string().min(6,{
+  password: z.string().min(6, {
     message: "Minimum 6 characters",
   }),
   name: z.string().min(1, {

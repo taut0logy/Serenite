af27eb0e0d7e8993c47a442e6145bfc51ff00f73
sakib <sakiburrahman11.msr@gmail.com>
2024-12-11 08:45:59 +0600

    Implement token retrieval and submission logic in new verification form

--- support-group/components/auth/new-verification-form.tsx
+++ support-group/components/auth/new-verification-form.tsx
@@ -1,8 +1,21 @@
 "use client"
 import { BeatLoader } from "react-spinners";
 import { CardWrapper } from '@/components/auth/card-wrapper';
+import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect } from "react";
 
 export const NewVerificationFrom = () => {
+
+  const searchParams = useSearchParams();
+  const token = searchParams.get('token');
+
+  const onSubmit = useCallback(() => {
+  }, [token]);
+
+  useEffect(()=> {
+    onSubmit();
+  }, [onSubmit]);
+
   return (
     <CardWrapper
     headerLabel="Confirming your verification"
